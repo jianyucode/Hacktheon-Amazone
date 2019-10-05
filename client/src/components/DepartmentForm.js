2f20860deb548aa82a747570819f50f878f1b2f3
Samantha Malay <samaleemalay@gmail.com>
2019-10-05 11:34:55 -0600

    Form

---
+++ client/src/components/DepartmentForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
+
+class DepartmentForm extends Component {
+  state = { name: '' }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const department = { ...this.state }
+    this.props.updateDepartment(department)
+  }
+
+  handleSubmit = (e, { name, value }) => {
+    e.preventDefault()
+    this.props.addItem(this.state)
+    this.setState( {name: '' })
+  }
+
+  render() {
+      return (
+    <Form onSubmit={this.handleSubmit}>
+      <Form.Input
+        label="New Department"
+        type="text"
+        name="department"
+        value={department}
+        onChange={this.handleChange}
+      />
+      <Form.Button color="blue">Save</Form.Button>
+    </Form>
+    )
+  }
+}
+
+export default DepartmentForm;
