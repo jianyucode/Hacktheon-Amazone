8e1b0c6c5b35433f234694305af5fa5e81d829a9
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-10-05 11:32:12 -0600

    finished department controller

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -1,2 +1,46 @@
 class Api::DepartmentsController < ApplicationController
+    before_action :set_user
+    before_action :set_department
+
+    def index
+        render json: User.deparment.all
+    end
+
+    def create
+        @department = User.deparment.new(deparment_params)
+        if @department.save
+            render json: @department
+        else
+            render json: {errors: @department.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @department = User.deparment.find(params[:id])
+        if @department.update(deparment_params)
+           render json: @department
+        else
+            render json: {errors: @department.errors}, status: :unprocessable_entity
+        end 
+    end
+
+    def destroy
+        User.department.find(params[:id]).destroy
+        render json: {message: 'department deleted'}
+    end
+
+
+    private
+    def deparment_params
+        params.require(:deparment).permit(:title)
+    end 
+
+    def set_department
+        @deparment = User.deparment.find(params[:id])
+    end
+
+    def set_user
+        @user = User.find(params[:user_id])
+    end
+
 end
